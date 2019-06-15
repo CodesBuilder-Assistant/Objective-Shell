4dfb634830b3a5d2a21b2b36792e3b6c9762a125
CodesBuilder <2602783536@qq.com>
2019-06-16 00:15:26 +0800

    Update efi_boot.c

--- src/bootmedia/efi_boot.c
+++ src/bootmedia/efi_boot.c
@@ -1,5 +1,6 @@
 #include <efi.h>
 EFI_STATUS main(EFI_HANDLE ImageHandle,EFI_SYSTEM_TABLE SystemTable)
 {
-
+    SystemTable->ConOut->OutputString(SystemTable->ConOut,"Starting Objective Shell Boot Media...\n");
+    return EFI_SUCCESS;
 }
