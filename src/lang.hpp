10dc34cd50f9de43f9fd0d781a8fdf89e5dfc8db
CodesBuilder <2602783536@qq.com>
2019-06-17 23:31:59 +0800

    Update lang.hpp

--- src/lang.hpp
+++ src/lang.hpp
@@ -9,13 +9,14 @@
 #include <conio.h>
 #include <windows.h>
 #endif
-#define LANGUAGE_ID_MAX 3
-char *language_select_menu_title[]={"en-us","zh-hans","zh-hant"};
+#define LANGUAGE_ID_MAX 5
+char *language_select_menu_titles[]={"en-us","zh-hans","zh-hant"};
 enum language_id
 {
     EN_US=0,
     ZH_HANS,
-    ZH_HANT
+    ZH_HANT,
+    AUTO_DETECT
 };
 unsigned short current_selected_lang_id=EN_US;
 __fastcall void ReadLanguageFile(unsigned short lang_id)
@@ -28,37 +29,64 @@ __fastcall void ReadLanguageFile(unsigned short lang_id)
 void ShowLanguageSetting(void)
 {
     puts("Language Setting\n");
-    puts("=====================");
     #ifdef __linux
     #elif defined(_WIN32)||defined(_WIN64)
-    int user_kbhit;
-    unsigned short selected_lang_id=current_selected_lang_id;
+    int gotkey,selected=0;
     while(true)
     {
-        for(int i=0;i<LANGUAGE_ID_MAX;i++)
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),BACKGROUND_BLUE|BACKGROUND_INTENSITY);
+        printf(".----------");
+        if(selected!=-1)
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),BACKGROUND_BLUE|BACKGROUND_INTENSITY);
+        else
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),BACKGROUND_RED|BACKGROUND_INTENSITY);
+        puts("|x|");
+        if(selected<4)
+            for(int i=0;i<4;i++)
+            {
+                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),BACKGROUND_BLUE|BACKGROUND_INTENSITY);
+				printf("|");
+				if(i==selected)
+					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),BACKGROUND_RED|BACKGROUND_GREEN|BACKGROUND_INTENSITY);
+				else
+					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),BACKGROUND_BLUE|BACKGROUND_INTENSITY|FOREGROUND_BLUE);
+				printf("%-12s",language_select_menu_titles[i]);
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),BACKGROUND_BLUE|BACKGROUND_INTENSITY);
+				printf("|");
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),BACKGROUND_INTENSITY);
+				puts(" ");
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_RED|FOREGROUND_GREEN|FOREGROUND_BLUE);
+            }
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),BACKGROUND_BLUE|BACKGROUND_INTENSITY);
+		printf("'------------'");
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),BACKGROUND_INTENSITY);
+		puts(" ");
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_RED|FOREGROUND_GREEN|FOREGROUND_BLUE);
+		printf(" ");
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),BACKGROUND_INTENSITY);
+		puts("              ");
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_RED|FOREGROUND_GREEN|FOREGROUND_BLUE);
+        gotkey=getch();
+        switch(gotkey)
         {
-            if(current_selected_lang_id==i)
-                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),BACKGROUND_RED|BACKGROUND_GREEN|BACKGROUND_BLUE);
-            printf("%s\n",language_select_menu_title[i]);
-            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_RED|FOREGROUND_GREEN|FOREGROUND_BLUE);
-        }
-        puts("=====================");
-        switch((user_kbhit=getch()))
-        {
-            case VK_UP:
-                selected_lang_id--;
+            case 72:
+                if(selected>=0)
+                    selected--;
                 break;
-            case VK_DOWN:
-                selected_lang_id++;
+            case 80:
+                if(selected<LANGUAGE_ID_MAX-1)
+                    selected++;
                 break;
             case VK_RETURN:
-                goto SetupLanguage;
+                if(selected==-1)
+                {
+                    system("cls");
+                    return;
+                }
                 break;
         }
+        system("cls");
     }
-    SetupLanguage:
-    system("cls");
-    current_selected_lang_id=selected_lang_id;
     #endif
 }
 #endif
