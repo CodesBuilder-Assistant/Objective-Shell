a4e45efa63b0b5d106f19479f765afe994768701
CodesBuilder <2602783536@qq.com>
2019-07-07 10:35:53 +0900

    Update debian.sh

--- install/linux/debian.sh
+++ install/linux/debian.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
 su root
 if [$? -eq 0 ]
     echo "\033[0m[\033[31mError\033[0m]Switch to root user failed"
 fi
-filename=objshell-current.tar.gz
-wget -N -T=30 https://github.com/CodesBuilder/Objective-Shell/archive/$filename
+#ver=
+filename=objshell-current.deb
+wget -N -T=30 https://github.com/CodesBuilder/Objective-Shell/archive/tags/$ver/$filename
 dpkg -i $filename
-rm -f objshell-current.tar.gz
+rm -f $filename
 exit
