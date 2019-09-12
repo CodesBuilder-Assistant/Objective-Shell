32c13c2d7f78574c4e8478091ed7c8490ad4594f
潜行的苦力怕 <2602783536@qq.com>
2019-09-12 17:52:14 +0800

    Update controls.hpp

--- include/windows/controls.hpp
+++ include/windows/controls.hpp
@@ -1,15 +1,49 @@
 #ifndef CONTROLS_HPP
 #define CONTROLS_HPP
 #include <Windows.h>
+#include <list>
+using std::list;
 
-IMGBUTTON current_this_ptr;
+extern LRESULT CALLBACK ImageButtonProc(HWND hwnd,UINT uMsg,WPARAM wParam,LPARAM lParam);
+
+bool imgbutton_class_registered=false;
+
+WNDCLASSW __imgbutton__;
+
+void RegisterImageButtonClass(void)
+{
+    if(!imgbutton_class_registered)
+    {
+        __imgbutton__={};
+        imgbutton_class_registered=true;
+    }
+}
+
+class IMGBUTTON
+{
+    private:
+        HMENU return_value;
+        HWND button_handle;
+        HWND parent_handle;
+        friend LRESULT CALLBACK ImageButtonProc(HWND hwnd,UINT uMsg,WPARAM wParam,LPARAM lParam);
+        POINT button_position;
+    public:
+        IMGBUTTON(void)
+        {
+            RegisterImageButtonClass();
+        }
+        IMGBUTTON(UINT x,UINT y,WORD width,WORD height,HMENU return_menu_value,HWND parent_window)
+        {
+            RegisterImageButtonClass();
+        }
+};
 
 LRESULT CALLBACK ImageButtonProc(HWND hwnd,UINT uMsg,WPARAM wParam,LPARAM lParam)
 {
     switch(uMsg)
     {
         case WM_CREATE:
-
+            
         case WM_LBUTTONDBLCLK:
             SendMessageW(GetParent(hwnd),WM_COMMAND,NULL,);
             break;
@@ -17,13 +51,4 @@ LRESULT CALLBACK ImageButtonProc(HWND hwnd,UINT uMsg,WPARAM wParam,LPARAM lParam
     return DefWindowProc(hwnd,uMsg,wParam,lParam);
 }
 
-class IMGBUTTON
-{
-    private:
-        HMENU return_value;
-    public:
-        IMGBUTTON(void)
-        {
-        }
-};
 #endif
