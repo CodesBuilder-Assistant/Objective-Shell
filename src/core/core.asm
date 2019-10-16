93bac26099cc8f43c4df614ddeb1ae6b09fe9508
潜行的苦力怕 <2602783536@qq.com>
2019-10-16 14:55:12 +0800

    Update core.asm

--- src/core/core.asm
+++ src/core/core.asm
@@ -1,5 +1,6 @@
 data segment
 MARK_OF_GENUINE db '...OBJSHELLCOREDOS...','$'
+user_input db 8193 dup (0)
 data ends
 code segment
 assume cs:code,ds:data
@@ -21,6 +22,20 @@ print:
     ret
 reboot:
     int 19h
+init:
+    mov ax,offset user_input
+    mov bx,offset user_input
+    add bx,8191
+    init_user_input_buffer:
+    mov [ax],'0'
+    cmp ax,bx
+    inc ax
+    jne init_user_input_buffer
+    mov [ax],'$'
+    xor ax,ax
+    xor bx,bx
+    xor cx,cx
+    xor dx,dx
 main:
 code ends
 end main
