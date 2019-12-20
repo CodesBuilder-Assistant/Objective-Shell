0fe471376217987f62c3a2677c664ec1c6108162
潜行的苦力怕 <59076353+CodesBuilder-Assistant@users.noreply.github.com>
2019-12-20 13:32:44 +0800

    Create str.hpp

---
+++ include/str.hpp
@@ -0,0 +1,12 @@
+#ifndef STR_HPP
+#define STR_HPP
+#include <string.h>
+#include <wchar.h>
+#include <string>
+using std::string;
+using std::wstring;
+
+bool strmatch(const wchar_t *match_source,const wchar_t *match_target)
+{
+}
+#endif
